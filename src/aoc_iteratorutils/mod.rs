abd6bd6e423e613da6835ef7ba8e9bcfa081f35e
Henrik Lundberg <velfolt@Mac.localdomain>
2025-12-01 14:37:47 +0100

    day1

---
+++ src/aoc_iteratorutils/mod.rs
@@ -0,0 +1,34 @@
+use std::{fmt::Debug, fs::read_to_string};
+
+pub struct InspectPrint<I: Iterator> {
+    iter: I,
+}
+
+impl<I: Iterator> Iterator for InspectPrint<I>
+where
+    I::Item: Debug,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(item) = self.iter.next() {
+            println!("{:?}", item);
+
+            return Some(item);
+        }
+
+        None
+    }
+}
+
+pub trait AdventOfCodeIteratorUtils: Iterator {
+    fn println(self) -> InspectPrint<Self>
+    where
+        Self: Sized,
+        Self::Item: Debug,
+    {
+        InspectPrint { iter: self }
+    }
+}
+
+impl<I: Iterator> AdventOfCodeIteratorUtils for I {}
---
+++ src/main.rs
@@ -0,0 +1,130 @@
+use std::fs::read_to_string;
+
+fn main() {
+    day1_part1();
+    day1_part2();
+}
+
+fn day1_part1() {
+    let file_input = read_to_string("input/day1.txt").unwrap();
+
+    let turns = file_input.trim().split("\n").map(|x| x.split_at(1));
+
+    let mut dial = 50;
+
+    let mut count = 0;
+
+    for (c, val) in turns {
+        let turn: i64 = val.parse().unwrap();
+        match c {
+            "L" => dial -= turn,
+            _ => dial += turn,
+        }
+        dial = dial.strict_rem_euclid(100);
+        if dial == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{:?}", count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_boundaries() {
+        assert_eq!(click("L50"), 1);
+        assert_eq!(
+            click(
+                "L49
+L1"
+            ),
+            1
+        );
+        assert_eq!(
+            click(
+                "L51
+"
+            ),
+            1
+        );
+        assert_eq!(click("L150"), 2);
+        assert_eq!(click("L151"), 2);
+        assert_eq!(click("L250"), 3);
+        assert_eq!(click("L251"), 3);
+
+        assert_eq!(click("R50"), 1);
+        assert_eq!(
+            click(
+                "R49
+R1"
+            ),
+            1
+        );
+        assert_eq!(
+            click(
+                "R51
+"
+            ),
+            1
+        );
+        assert_eq!(click("R150"), 2);
+        assert_eq!(click("R151"), 2);
+        assert_eq!(click("R250"), 3);
+        assert_eq!(click("R251"), 3);
+    }
+}
+
+fn click(input: &str) -> i64 {
+    let turns = input.trim().split("\n").map(|x| x.split_at(1));
+
+    let mut dial = 50;
+    let mut count = 0;
+
+    for (c, val) in turns {
+        let turn: i64 = val.parse().unwrap();
+
+        let start_dial = dial;
+
+        match c {
+            "L" => dial -= turn,
+            _ => dial += turn,
+        }
+
+        if start_dial == 0 && dial < 0 {
+            count += dial / -100;
+
+            if dial.rem_euclid(100) == 0 {
+                count -= 1;
+            }
+        } else if start_dial != 0 && dial < 0 {
+            count += dial / -100 + 1;
+
+            if dial.rem_euclid(100) == 0 {
+                count -= 1;
+            }
+        } else if dial >= 100 {
+            count += dial / 100;
+
+            if dial.rem_euclid(100) == 0 {
+                count -= 1;
+            }
+        }
+
+        dial = dial.strict_rem_euclid(100);
+        if dial == 0 {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn day1_part2() {
+    let file_input = read_to_string("input/day1.txt").unwrap();
+    let count = click(&file_input);
+
+    println!("{:?}", count);
+}
