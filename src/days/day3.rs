6861abf35d2ba2a5b4bbfa93462fe94c2334e151
Henrik Lundberg <velfolt@Mac.localdomain>
2025-12-03 08:53:46 +0100

    clean up day3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -9,39 +9,39 @@ pub struct Day3;
 impl AocDay for Day3 {
     fn part1() {
         let file_input = read_to_string("input/day3.txt").unwrap();
-        let joltage = joltage(&file_input, 2);
+        let joltage = largest_joltage(&file_input, 2);
         println!("{joltage}")
     }
 
     fn part2() {
         let file_input = read_to_string("input/day3.txt").unwrap();
-        let joltage = joltage(&file_input, 12);
+        let joltage = largest_joltage(&file_input, 12);
         println!("{joltage}")
     }
 }
 
-fn joltage(input: &str, n: usize) -> i64 {
+fn largest_joltage(input: &str, batteries: usize) -> i64 {
     input
         .trim()
         .lines()
         .map(|l| {
-            let mut number = 0;
-            let mut temp = 0;
-            let mut i1 = 0;
+            let mut largest_joltage = 0;
+            let mut index = 0;
 
-            for jolt_index in (0..n).rev() {
-                for i in i1..(l.len() - jolt_index) {
-                    if l[i..i + 1].parse::<i64>().unwrap() > temp {
-                        temp = l[i..i + 1].parse::<i64>().unwrap();
-                        i1 = i + 1;
+            for battery_index in (0..batteries).rev() {
+                let mut max_in_sequence = 0;
+                for i in index..(l.len() - battery_index) {
+                    let value = l.as_bytes()[i] as i64 - '0' as i64;
+                    if value > max_in_sequence {
+                        max_in_sequence = value;
+                        index = i + 1;
                     }
                 }
 
-                number += temp * 10_i64.pow(jolt_index as u32);
-                temp = 0;
+                largest_joltage += max_in_sequence * 10_i64.pow(battery_index as u32);
             }
 
-            number
+            largest_joltage
         })
         .sum::<i64>()
 }
